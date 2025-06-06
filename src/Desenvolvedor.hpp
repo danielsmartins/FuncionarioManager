d2392909e95acef1d04861ef9cff9306a1698743
felipe-dsm <fdsouzamacedo@gmail.com>
2025-06-06 16:10:14 -0300

    Update Desenvolvedor.hpp

---
+++ src/Desenvolvedor.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "Funcionario.hpp"
+
+class Desenvolvedor : public Funcionario { 
+private:
+    int quantidadeDeProjetos; [cite: 5]
+
+public:
+    void setQuantidadeDeProjetos(int q) { quantidadeDeProjetos = q; }
+    int getQuantidadeDeProjetos() { return quantidadeDeProjetos; }
+
+    float calcularSalarioFinal() override;
+
+    void exibirInformacoes() override;
+};
