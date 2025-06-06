e3792fac563218e8ad5c3efda576822103546c46
felipe-dsm <fdsouzamacedo@gmail.com>
2025-06-06 16:24:49 -0300

    Update Desenvolvedor.cpp

--- src/Desenvolvedor.cpp
+++ src/Desenvolvedor.cpp
@@ -1,3 +1,16 @@
 #include "src/Funcionario.hpp"
 #include <iostream>
 
+Desenvolvedor::setQuantidadeDeProjetos(int q){
+  quantidadeDeProjetos = q;
+}
+
+Desenvolvedor::getQuantidadeDeProjetos(){
+  return quantidadeDeProjetos;
+}
+
+Desenvolvedor::calcularSalarioFinal(){
+}
+
+Desenvolvedor::exibirInformacoes(){
+}
